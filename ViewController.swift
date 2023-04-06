2517f78f90fbe5391310ad3a8575e43384a982be
NinPala <sibuyajapan30@gmail.com>
2023-04-06 12:00:14 +0900

    First commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalenderApp
+//
+//  Created by 小松周平 on 2023/02/05.
+//
+
+import UIKit
+import FSCalendar
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var calender: FSCalendar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+}
